a9fd80a9cd7e17ed0878f9d81cc5e0f984fae520
seboschtion <sebastian.hug@protonmail.com>
2018-03-08 11:34:11 +0100

    added stylesheet

--- react/src/components/TransactionOverview.js
+++ react/src/components/TransactionOverview.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React from 'react'
+import { Table } from 'semantic-ui-react'
 
 class TransactionOverview extends React.Component {
     render() {
         return (
-            <div>
-                She walkted into a bar and left it drunk.
-            </div>
+            <Table celled>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Datum</Table.HeaderCell>
+                        <Table.HeaderCell>Von</Table.HeaderCell>
+                        <Table.HeaderCell>Zu</Table.HeaderCell>
+                        <Table.HeaderCell>Betrag</Table.HeaderCell>
+                        <Table.HeaderCell>Kontostand neu</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    <Table.Row>
+                        <Table.Cell>8. März 2018</Table.Cell>
+                        <Table.Cell>Mario</Table.Cell>
+                        <Table.Cell>Sebastian</Table.Cell>
+                        <Table.Cell>1'000'000 Fr.</Table.Cell>
+                        <Table.Cell>2'000'000 Fr.</Table.Cell>
+                    </Table.Row>
+                </Table.Body>
+            </Table>
         );
     }
 }
