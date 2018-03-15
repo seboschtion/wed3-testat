f7066072144e01e05b0b5e6390faf1a8cc5aedfc
seboschtion <sebastian.hug@protonmail.com>
2018-03-15 11:52:30 +0100

    date format

--- react/src/components/TransactionOverviewRows.js
+++ react/src/components/TransactionOverviewRows.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Table } from 'semantic-ui-react'
+import moment from 'moment'
 
 function TransactionOverviewRows({ transactions }) {
     const renderRow = function ({from, target, amount, total, date}) {
         return (
             <Table.Row>
-                <Table.Cell>{date}</Table.Cell>
+                <Table.Cell>{moment(date).format("DD.MM.YYYY")}</Table.Cell>
                 <Table.Cell>{from}</Table.Cell>
                 <Table.Cell>{target}</Table.Cell>
                 <Table.Cell>{amount.toFixed(2)}</Table.Cell>
