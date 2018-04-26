a9a6642308fe063d599a61d1f4aa23c09c0de922
Mario Tarreghetta <mario.tarreghetta@hsr.ch>
2018-04-26 11:43:14 +0200

    Added transaction overview

--- angular/src/app/shared/shared.module.ts
+++ angular/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {BoxComponent} from './box/box.component';
+import {BoxComponent } from './box/box.component';
+import {CommonModule} from "@angular/common";
 
 const EXPORTED_DECLARATIONS = [
   // TODO: Add declarations here, if additional components/directives/... should be exported
@@ -11,8 +12,9 @@ const INTERNAL_DECLARATIONS = [
   // Declarations (Components / Directives) which can be used inside the Module
 ];
 const EXPORTS = [
-  ...EXPORTED_DECLARATIONS
+  ...EXPORTED_DECLARATIONS,
   // TODO: Add exports here, if additional modules should be exported
+  CommonModule, FormsModule
 ];
 
 @NgModule({
