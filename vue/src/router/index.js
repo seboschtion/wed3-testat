dac04aece65183b56f137159b95c36e7ce5bb5cb
Mario Tarreghetta <mario.tarreghetta@hsr.ch>
2018-05-03 10:32:17 +0200

    fixed eslint warnings

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -10,34 +10,34 @@ Vue.use(Router);
 const auth = new Auth();
 
 function requireAuth(to, from, next) {
-    if(!auth.isAuthenticated()) {
-        next({
-            path: '/'
-        })
-        return;
-    }
-    next();
+  if (!auth.isAuthenticated()) {
+    next({
+      path: '/',
+    });
+    return;
+  }
+  next();
 }
 
 function redirectHome(to, from, next) {
-    next({ path: auth.isAuthenticated() ? '/dashboard' : 'login' });
+  next({ path: auth.isAuthenticated() ? '/dashboard' : 'login' });
 }
 
 export default new Router({
-    routes: [{
-        path: '/',
-        beforeEnter: redirectHome
-    }, {
-        path: '/login',
-        component: Login,
-    }, {
-        path: '/register',
-        component: Registration,
-    }, {
-        path: '/dashboard',
-        component: Dashboard,
-        beforeEnter: requireAuth
-    }],
+  routes: [{
+    path: '/',
+    beforeEnter: redirectHome,
+  }, {
+    path: '/login',
+    component: Login,
+  }, {
+    path: '/register',
+    component: Registration,
+  }, {
+    path: '/dashboard',
+    component: Dashboard,
+    beforeEnter: requireAuth,
+  }],
 });
 
 // https://github.com/vuejs/vue-router/tree/dev/examples
