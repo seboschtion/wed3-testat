28c7b04cacd10ee18992ef9ccd7e7372130ca966
marodev <martin.odermatt@hsr.ch>
2018-05-03 13:43:38 +0200

    added payment form

--- angular/src/app/dashboard/components/payment/payment.component.ts
+++ angular/src/app/dashboard/components/payment/payment.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {BankAccountService} from '../../services';
 import {BankAccount} from '../../models/bankaccount';
+import {NgForm} from '@angular/forms';
+import {TransactionService} from '../../services/transaction.service';
+import {Transaction} from '../../models/transaction';
 
 @Component({
   selector: 'wed-payment',
@@ -9,17 +12,35 @@ import {BankAccount} from '../../models/bankaccount';
 })
 export class PaymentComponent implements OnInit {
 
-  constructor(private bankAccountService: BankAccountService) {
+  constructor(private bankAccountService: BankAccountService, private transactionService: TransactionService) {
   }
 
   public bankAccount: BankAccount;
+  private transaction: Transaction;
+  public target: string;
 
   ngOnInit() {
     const bankAccount = this.bankAccountService.getCurrentBankAccount().subscribe(value =>
       this.bankAccount = value
     );
-    console.log('bankAccount:');
-    console.log(bankAccount);
   }
 
+  public doPayment(f: NgForm): boolean {
+    if (f && f.valid) {
+      this.transactionService.submitTransaction(f.form.value.target, f.form.value.amount).subscribe(value => {
+          console.log(value); // TODO: remove log
+          if (!value) {
+            return false;
+          }
+            // TODO: display current transaction
+            this.transaction = value;
+            return true;
+        }
+      );
+    }
+    return false;
+  }
+  public searchBankaccount() {
+    console.log(this);
+  }
 }
