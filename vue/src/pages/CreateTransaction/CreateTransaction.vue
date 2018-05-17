ca0cafd626aa0aaa5f0a623343039b09daf1d923
Mario Tarreghetta <mario.tarreghetta@hsr.ch>
2018-05-17 11:04:51 +0200

    Added subcomponents

---
+++ vue/src/pages/CreateTransaction/CreateTransaction.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <form>
+      todo
+      <input/>
+      <input/>
+    </form>
+  </div>
+</template>
+
+<script src="./CreateTransaction.js"></script>
+<style src="./CreateTransaction.css"></style>
---
+++ vue/src/pages/NewTransaction/NewTransaction.js
@@ -0,0 +1,9 @@
+import CreateTransaction from '../../pages/CreateTransaction/CreateTransaction.vue';
+import TransactionOverview from '../../pages/TransactionOverview/TransactionOverview.vue';
+
+export default {
+  components: {
+    CreateTransaction,
+    TransactionOverview,
+  },
+}
