f07787d635e1c5accd3268a751def569ae41f76a
Mario Tarreghetta <mario.tarreghetta@hsr.ch>
2018-05-24 12:07:51 +0200

    added filters

--- vue/src/pages/TransactionOverview/TransactionOverview.vue
+++ vue/src/pages/TransactionOverview/TransactionOverview.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    <p>todo filters</p>
+    <div>
+      <h4>Filter</h4>
+
+      <p>Wähle ein Jahr</p>
+      <select v-model="selectedYear">
+        <option v-for="yearFilter in getYearFilters()" :value="yearFilter.value">
+          {{yearFilter.text}}
+        </option>
+      </select>
+
+      <p>Wähle einen Monat</p>
+      <select v-model="selectedMonth">
+        <option v-for="month in getMonthFilters()" :value="month.value">
+          {{month.text}}
+        </option>
+      </select>
+    </div>
     <TransactionList ></TransactionList>
   </div>
 </template>
