3e7488b2501e561466e92840e6699e4e0b89edad
Mario Tarreghetta <mario.tarreghetta@hsr.ch>
2018-05-27 14:14:30 +0200

    removed unused code

--- vue/src/components/Input/Input.vue
+++ vue/src/components/Input/Input.vue
@@ -6,7 +6,6 @@
         v-bind:value="value"
         v-bind:type="type"
         v-on:input="$emit('input', $event.target.value)"/>
-        <small v-if="condition" class="error">Die Passwörter stimmen nicht überein</small>
     </label>
   </div>
 </template>
