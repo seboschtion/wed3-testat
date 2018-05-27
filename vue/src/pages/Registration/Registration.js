3ad44bc44e4791e02fd9458a5bc42107df43cf56
martin odermatt <martin.odermatt@hsr.ch>
2018-05-27 14:06:33 +0200

    added vue.js registration validation

--- vue/src/pages/Registration/Registration.js
+++ vue/src/pages/Registration/Registration.js
@@ -23,9 +23,29 @@ export default {
       event.preventDefault();
 
       if (!this.firstname) { this.errorMessage = 'Bitte geben Sie einen Vornamen an'; return; }
+      if (!this.validateMinLength(this.firstname)) {
+        this.errorMessage = 'Vorname muss mindestens 3 Zeichen lang sein';
+        return;
+      }
+
       if (!this.lastname) { this.errorMessage = 'Bitte geben Sie einen Nachnamen an'; return; }
+      if (!this.validateMinLength(this.lastname)) {
+        this.errorMessage = 'Nachname muss mindestens 3 Zeichen lang sein';
+        return;
+      }
+
       if (!this.username) { this.errorMessage = 'Bitte geben Sie einen Benutzernamen an'; return; }
-      if (!this.username || this.password.length < 3) { this.errorMessage = 'Das Passwort muss mindestens drei Zeichen lang sein'; return; }
+      if (!this.validateMinLength(this.username)) {
+        this.errorMessage = 'Benutzername muss mindestens 3 Zeichen lang sein';
+        return;
+      }
+
+      if (!this.password) { this.errorMessage = 'Das Passwort muss mindestens drei Zeichen lang sein'; return; }
+      if (!this.validateMinLength(this.password)) {
+        this.errorMessage = 'Passwort muss mindestens 3 Zeichen lang sein';
+        return;
+      }
+
       if (this.password !== this.passwordConfirmation) { this.errorMessage = 'Die Passwörter stimmen nicht überein'; return; }
       this.errorMessage = '';
 
@@ -35,5 +55,6 @@ export default {
           .catch((e) => { this.errorMessage = e.toString(); }))
         .catch((e) => { this.errorMessage = e.toString(); });
     },
+    validateMinLength: value => value.length > 2,
   },
 };
